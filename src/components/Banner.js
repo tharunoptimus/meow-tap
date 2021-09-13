283b13aa6034f5bc68b37a1eaa5a333d1a04b0b3
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-09-13 14:33:41 +0530

    Added Typewriting effect to the banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,20 @@
+import Typewriter from "typewriter-effect"
 import "./css/Banner.css"
 
 const Banner = () => {
-    return (
-        <div className="bannerHolder">
-          <p>One App for every need.</p>  
-        </div>
-    );
+	return (
+		<div className="bannerHolder">
+			<p>
+            <Typewriter
+				onInit={(typewriter) => {
+					typewriter
+						.typeString("One App for every need.")
+						.start()
+				}}
+			/>
+            </p>
+		</div>
+	)
 }
 
-export default Banner;
+export default Banner
